bff9afbf87caa0a74ce3ab13614cd0a343e63ce9
vahaknp <vahakn@papazian.info>
2015-02-10 16:22:04 -0800

    require parseLedger payment function

--- api/routes/getPayments.js
+++ api/routes/getPayments.js
@@ -1,5 +1,6 @@
 var config   = require('../../config/api.config');
 var log      = require('../../lib/log')('api');
+var payment  = require('../../lib/ledgerParser/payment');
 var postgres = new require('../lib/db.js')(config.get('sql'));
 var response = require('response');
 
@@ -7,10 +8,76 @@ var getPayments = function (req, res, next) {
 
   var options = prepareOptions();
   
-  function prepareOptions() {
-    account : req.params.address
-  }
+  log.info('ACCOUNT TX:', options.account); 
 
+  postgres.getAccountTransactions(options, function(err, resp) {
+    if (err) {
+      errorResponse(err);   
+    } else {
+      console.log(resp.transactions[0]);
+    }
+  });
+  
+ /**
+  * prepareOptions
+  * parse request parameters to determine query options 
+  */
+  function prepareOptions () {
+    var options = {
+      account    : req.params.address,
+      limit      : req.query.limit || 20,
+      offset     : req.query.offset,
+      descending : req.query.descending === 'false' ? false : true,
+      start      : req.query.start,
+      end        : req.query.end,
+      minLedger  : req.query.ledger_min,
+      maxLedger  : req.query.ledger_max,
+      type       : req.query.type,
+      result     : req.query.result,
+      binary     : !req.query.binary || req.query.binary === 'false' ? false : true 
+    };
+    
+    if (isNaN(options.limit)) {
+      options.limit = 20;
+        
+    } else if (options.limit > 1000) {
+      options.limit = 1000;  
+    } 
+    
+    return options;
+  };
+  
+ /**
+  * errorResponse 
+  * return an error response
+  * @param {Object} err
+  */
+  function errorResponse (err) {
+    if (err.code === 400) {
+      log.error(err.error || err);
+      response.json({result:'error', message:err.error}).status(400).pipe(res);  
+       
+    } else {
+      response.json({result:'error', message:'unable to retrieve transactions'}).status(500).pipe(res);  
+    }     
+  };
+  
+ /**
+  * successResponse
+  * return a successful response
+  * @param {Object} transactions
+  */  
+  function successResponse (data) {
+    var result = {
+      result       : 'success',
+      count        : data.transactions.length,
+      total        : data.total,
+      transactions : data.transactions
+    };
+    
+    log.info('ACCOUNT TX: Transactions Found:', data.transactions.length);  
+    response.json(result).pipe(res);      
+  };
 }
 
 module.exports = getPayments;
